12c3321da23707507752c0df30640b5b307e760b
Lydia <lydia.lee@berkeley.edu>
2013-11-17 18:12:07 -0800

    20, possibly 18 Done
    Autograder's a little b and something seems to be wrong with it; 19 has a problem and makes 18 fail

--- questions.scm
+++ questions.scm
@@ -1,4 +1,6 @@
+#!scheme
 ; Some utility functions that you may find useful.
+(define nil '())
 (define (map proc items)
   (if (null? items)
       nil
@@ -23,8 +25,12 @@
 ;; Merge two lists LIST1 and LIST2 according to COMP and return
 ;; the merged lists.
 (define (merge comp list1 list2)
-    ; *** YOUR CODE HERE ***
-    nil)
+    (cond 
+        ((null? list1) list2)
+        ((null? list2) list1)
+        ((comp (car list1) (car list2)) (cons (car list1) (merge comp (cdr list1) list2)))
+        (else (cons (car list2) (merge comp (cdr list2) list1)))
+    ))
 
 (merge < '(1 5 7 9) '(4 8 10))
 ; expect (1 4 5 7 8 9 10)
@@ -109,8 +115,36 @@
 ;; Takes a TREE of numbers and outputs a list of sums from following each
 ;; possible path from root to leaf.
 (define (tree-sums tree)
-  ; *** YOUR CODE HERE ***
-  nil)
+  
+    (define (flatten lst)
+      ; Helper function--takes a list that potentially has lists
+      ; inside of it and flattens it
+
+        (define (flatten-complex lst)
+         (cond 
+           ((null? (cdr lst)) (car lst))
+           (else (append (flatten (car lst)) (flatten (cdr lst))))
+         )
+       )
+    
+       (define (no-nesting lst)
+         (cond 
+           ((null? lst) #t)
+           ((not (number? (car lst))) #f)
+           (else (no-nesting (cdr lst)))
+         )
+       )
+
+       (cond 
+         ((no-nesting lst) lst)
+         (else (flatten-complex lst))
+       )
+     )
+    (if (null? (children tree))
+        (entry tree)
+        (map (lambda (child) (+ child (entry tree))) (flatten (map tree-sums (children tree))))
+    )
+ )
 
 (tree-sums tree)
 ; expect (20 19 13 16 11)
